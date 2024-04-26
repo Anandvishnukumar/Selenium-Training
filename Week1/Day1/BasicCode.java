ce4af49da81e73e7808a614a4ed32707e76ceff6
Anandvishnukumar <167883383+Anandvishnukumar@users.noreply.github.com>
2024-04-26 11:14:33 +0530

    Add files via upload

---
+++ Week1/Day1/BasicCode.java
@@ -0,0 +1,12 @@
+package Week1.Day1;
+
+public class BasicCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome To Test Leaf Learning");
+
+	}
+
+}
